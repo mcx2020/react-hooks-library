b3ae7321d3a346e902c6d49f9685f6523f04591d
EddieMa <machaoxue@yeah.net>
2020-06-21 15:01:35 +0800

    useEffect 加一些注释

--- src/main.js
+++ src/main.js
@@ -23,16 +23,16 @@ function useState(initialValue) {
 // 空数组，只执行 1 次
 // 有值的数组，执行 1+ 次
 // 不写，执行 1+ 次
-let _deps = undefined // 记录上一次数组，第一次当然未定义
+let oldDeps = undefined // 记录上一次数组，第一次当然未定义
 function useEffect(callback, depArray) {
 	const hasDeps = depArray
 	// 空数组永远返回 true
 	// 有值的数组时，一一对比是否和上一次对比是否一变化，第一次对比一定返回 false，以后有变化时返回 false
-	// depArray.every((array,i)=>{depArray[i]===_deps[i]}  
-	const hasChangedDeps = _deps ? !depArray.every((array, i) => array === _deps[i]) : true
+	// depArray.every((item,i)=>item === oldDeps[i]}  
+	const hasChangedDeps = oldDeps ? !depArray.every((item, i) => item === oldDeps[i]) : true
 	if (hasChangedDeps || !hasDeps) {
 		callback()
-		_deps = depArray
+		oldDeps = depArray
 	}
 }
 
@@ -40,7 +40,7 @@ const App = () => {
 	const [count, setCount] = useState(0)
 	useEffect(() => {
 		console.log(`挂载时：${count}`)
-	}, [])
+	}, [count])
 	return (
 		<div>
 			<div>{count}</div>
